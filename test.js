ee36a6a94e50f6e979941cf2a484bd9fb00c36d1
Adam Blackburn <regality@gmail.com>
2013-02-22 15:56:34 -0700

    converting to mocha

--- test.js
+++ test.js
@@ -1,61 +1,74 @@
 var waitress = require('./index')
   , assert = require('assert');
 
-assert.throws(function() {
-  var done = waitress(3, function(err) {
-    if (err) throw err;
+describe('waitress', function() {
+  it('should recieve an error', function() {
+    assert.throws(function() {
+      var done = waitress(3, function(err) {
+        if (err) throw err;
+      });
+
+      done();
+      done();
+      done(new Error);
+    });
   });
 
-  done();
-  done();
-  done(new Error);
+  it('should receive error from single callback', function() {
+    assert.throws(function() {
+      var done = waitress(1, function(err) {
+        if (err) throw err;
+      });
+
+      done(new Error('hai tai mai shu'));
+    }, /hai tai mai shu/);
+  });
+
+  it('should execute cb with zero count', function(next) {
+    // for zero args
+    var done = waitress(0, function(err) {
+      assert.ok(!err);
+      next();
+    });
+  });
+
+  it('should ignore false error param', function() {
+    assert.doesNotThrow(function() {
+      var done = waitress(3, function(err) {
+        if (err) throw err;
+      });
+
+      done();
+      done();
+      done();
+      done(false);
+    });
+  });
+
+  it('should return last error', function() {
+    assert.throws(function() {
+      var done = waitress(3, function(err, results) {
+        if (err) throw err;
+      });
+
+      done();
+      done(new Error("nein nein nein"));
+      done(new Error("no no no"));
+    }, /no no no/);
+  });
+
+  it('should aggregate results', function() {
+    (function() {
+      var done = waitress(3, function(err, results) {
+        if (err) throw err;
+        assert.deepEqual(results, [1, 2, 3], "results aggregation wrong");
+      });
+
+      done(null, 1);
+      done(null, 2);
+      done(null, 3);
+      done(null, 4);
+    })();
+  });
+
 });
-
-assert.throws(function() {
-  var done = waitress(1, function(err) {
-    if (err) throw err;
-  });
-
-  done(new Error('hai tai mai shu'));
-}, /hai tai mai shu/);
-
-// for zero args
-var done = waitress(0, function(err) {
-  assert.ok(!err);
-});
-
-assert.doesNotThrow(function() {
-  var done = waitress(3, function(err) {
-    if (err) throw err;
-  });
-
-  done();
-  done();
-  done();
-  done(false);
-});
-
-assert.throws(function() {
-  var done = waitress(3, function(err, results) {
-    if (err) throw err;
-  });
-
-  done();
-  done(new Error("nein nein nein"));
-  done(new Error("no no no"));
-}, /no no no/);
-
-(function() {
-  var done = waitress(3, function(err, results) {
-    if (err) throw err;
-    assert.deepEqual(results, [1, 2, 3], "results aggregation wrong");
-  });
-
-  done(null, 1);
-  done(null, 2);
-  done(null, 3);
-  done(null, 4);
-})();
-
-console.log("No errors thrown, all tests must have passed.");
-
